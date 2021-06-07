a1d8543bc80d73eb38293bce5f2d80c2c366b888
Héctor Realpe <hrealpe@gmail.com>
2021-06-07 13:48:38 +0200

    lunes 13:49

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaLibrosComponent } from './pages/pagina-libros/pagina-libros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaLibrosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/pagina-libros/pagina-libros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-libros',
+  templateUrl: './pagina-libros.component.html',
+  styleUrls: ['./pagina-libros.component.css']
+})
+export class PaginaLibrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
