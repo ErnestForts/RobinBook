63e365c846c329da915a5b7cefd3cbd3d751cfd9
barbosound <barbo_94@hotmail.com>
2021-06-07 17:55:13 +0200

    login master
    Vista master para LOGIN, REGISTER FORGOT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './pages/login-form/login-form.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterFormComponent } from './pages/register-form/register-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent,
+    children: [
+    {
+      outlet: 'login',
+      path: '',
+      component: LoginFormComponent
+    }
+    ]
+  },
+  {path: 'register', component: LoginComponent,
+    children: [
+    {
+      outlet: 'register',
+      path: '',
+      component: RegisterFormComponent
+    }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LoginFormComponent } from './pages/login-form/login-form.component';
+import { RegisterFormComponent } from './pages/register-form/register-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
