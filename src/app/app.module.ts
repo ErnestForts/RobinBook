c345807da7711e3b2d830d16f4264c9a453056ef
ErnestForts <ernest.forts@eram.cat>
2021-06-08 15:01:50 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapaComponent } from './pages/mapa/mapa.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { FooterVacioComponent } from './components/footer-vacio/footer-vacio.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderMapasComponent } from './components/header-mapas/header-mapas.component';
+import { RankingComponent } from './page/ranking/ranking.component';
+import { HeaderRankingComponent } from './components/header-ranking/header-ranking.component';
+import { MapaComponent } from './pages/mapa/mapa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RankingComponent,
+    FooterComponent,
+    FooterVacioComponent,
+    HeaderMapasComponent,
+    HeaderRankingComponent,
     MapaComponent,
     FooterComponent,
     FooterVacioComponent,
