746ffd7ead2b74ab82aa9ce2c03bbf6400c9bfe5
barbosound <barbo_94@hotmail.com>
2021-06-10 19:07:39 +0200

    intento de generic component

--- src/app/components/header-favoritos/header-favoritos.component.ts
+++ src/app/components/header-favoritos/header-favoritos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-header-favoritos',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderFavoritosComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+    title;
+  @Input()
+  id;
+  @Input()
+  src;
+  @Input()
+  alt;
+  constructor(private _location: Location) { }
 
   ngOnInit(): void {
+  }
+
+  backClicked() {
+    this._location.back();
   }
 
 }
