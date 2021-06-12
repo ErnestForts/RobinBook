a9119d82dd266e7c5394af19b74bc234d72608eb
Héctor Realpe <hrealpe@gmail.com>
2021-06-12 19:20:50 +0200

    nosotros, footer y header corregidos

--- src/app/components/header-lugar/header-lugar.component.ts
+++ src/app/components/header-lugar/header-lugar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header-lugar',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderLugarComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  title;
+  @Input()
+  id;
+  @Input()
+  src;
+  @Input()
+  alt;
+
+  constructor(public _location: Location) { }
 
   ngOnInit(): void {
   }
