95ad61cb96ee998a1cee3c51af4500c4d70a9a2f
Joba Lopez <jobalc.94@outlook.es>
2021-06-15 16:42:13 +0200

    Cambios footer y mapa

--- src/app/services/mapa-servicio.service.ts
+++ src/app/services/mapa-servicio.service.ts
@@ -2,60 +2,47 @@ import { Injectable } from '@angular/core';
 
 import Map from 'ol/Map';
 import View from 'ol/View';
-import VectorLayer from 'ol/layer/Vector';
-import Style from 'ol/style/Style';
+// import VectorLayer from 'ol/layer/Vector';
+// import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
-import OSM from 'ol/source/OSM';
+// import OSM from 'ol/source/OSM';
 import * as olProj from 'ol/proj';
-import TileLayer from 'ol/layer/Tile';
-import { Feature } from 'ol';
+// import TileLayer from 'ol/layer/Tile';
+import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
-import VectorSource from 'ol/source/Vector';
+// import VectorSource from 'ol/source/Vector';
 import TileJSON from 'ol/source/TileJSON';
+import Geolocation from 'ol/Geolocation';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import { Overlay } from 'ol';
+import Style from 'ol/style/Style';
+import Projection from 'ol/proj/Projection';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapaServicioService {
 
-  public mapa : Map;
+  public map : Map;
   punto1;
   punto2;
   vectorSource;
   vectorLayer;
   rasterLayer;
+  geolocation;
 
   constructor() { }
 
   initializeMap(mapa : Map) {
 
-    // this.punto1.setStyle(new Style({
-    //   image: new Icon(({
-    //     color: '#8959A8',
-    //     crossOrigin: 'anonymous',
-    //     src: 'src\assets\icons\image.svg',
-    //     imgSize: [20, 20]
-    //   }))
-    // }))
 
-    // this.punto1 = new Feature({
-    //   geometry: new Point(olProj.fromLonLat([-3.7, 40.41]))
-    // });
-
-    // this.vectorSource = new VectorSource({
-    //   features: [this.punto1, this.punto2]
-    // });
-
-    // this.vectorLayer = new VectorLayer({
-    //   source: this.vectorSource
-    // });
-
-    // this.rasterLayer = new TileLayer({
-    //   source: new TileJSON({
-    //     url: 'https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure',
-    //     crossOrigin: ''
-    //   })
-    // });
+    const view= new View({
+      projection: 'EPSG:4326',
+      center: olProj.fromLonLat([-3.7, 40.41]),
+      zoom: 5
+    });
 
     mapa = new Map({
       target: 'mapa',
@@ -64,11 +51,22 @@ export class MapaServicioService {
           source: new OSM()
         })
       ],
-      view: new View({
-        center: olProj.fromLonLat([-3.7, 40.41]),
-        zoom: 5
-      })
+      view: view
+
     });
+
+    this.vectorSource = new VectorSource({});
+    this.vectorLayer = new VectorLayer({source: this.vectorSource});
+
+    let marker = new Feature({
+      geometry: new Point([-3.7, 40.41])
+    }); 
+
+    marker.getGeometry().transform(new Projection({code: 'EPSG:4326'}), new Projection({code: 'EPSG:4326'}));
+
+    this.vectorSource.addFeature(marker);
+
+    mapa.addLayer(this.vectorLayer);
 
   }
 
