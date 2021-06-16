3552127836aa44bc009bf13f4a5208c8f4f4f7e6
Héctor Realpe <hrealpe@gmail.com>
2021-06-16 10:58:57 +0200

    clase libro creada

---
+++ src/app/models/libro/libro.ts
@@ -0,0 +1,9 @@
+export class Libro {
+    public liro_id : number;
+    public Titulo : string;
+    public Autor : string;
+    public Description : string;
+    public Foto: string;
+
+    constructor(){ }
+}
