2d7efb1b1fa31a182d6ef3d5cc28ffae722d663b
Héctor Realpe <hrealpe@gmail.com>
2021-06-22 11:00:06 +0200

    cambios menores

--- src/app/models/comentsLibros/rawcoments.ts
+++ src/app/models/comentsLibros/rawcoments.ts
@@ -2,8 +2,9 @@ export class Rawcoments {
     public id_Libro: string;
     public id_User: string;
     public Coment: string;
+    public id_ComentLibro?: string;
 
-    constructor(id_Libro: string, id_User: string, Coment: string){ 
+    constructor(id_Libro: string, id_User: string, Coment: string, id_ComentLibro?: string){ 
         this.id_Libro = id_Libro;
         this.id_User = id_User;
         this.Coment = Coment;
