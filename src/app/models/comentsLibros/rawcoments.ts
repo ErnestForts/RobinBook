3c5d9eddf87314c031e6fa3b9464914a058c3ed4
Héctor Realpe <hrealpe@gmail.com>
2021-06-22 12:01:45 +0200

    bindeado contador de likes

--- src/app/models/comentsLibros/rawcoments.ts
+++ src/app/models/comentsLibros/rawcoments.ts
@@ -3,8 +3,9 @@ export class Rawcoments {
     public id_User: string;
     public Coment: string;
     public id_ComentLibro?: string;
+    public likeComent?: string;
 
-    constructor(id_Libro: string, id_User: string, Coment: string, id_ComentLibro?: string){ 
+    constructor(id_Libro: string, id_User: string, Coment: string, id_ComentLibro?: string, likeComent?: string){ 
         this.id_Libro = id_Libro;
         this.id_User = id_User;
         this.Coment = Coment;
