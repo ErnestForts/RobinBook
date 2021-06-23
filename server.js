63d205087e3a2debfdae18e3a4db95d58368b0d7
barbosound <barbo_94@hotmail.com>
2021-06-23 11:57:55 +0200

    add app name in config heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/robinbook'));
 // Start the app by listening on the default
 // Heroku port
 
@@ -20,6 +20,6 @@ const forceSSL = function() {
 app.use(forceSSL());
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/robinbook/index.html'));
 });
 app.listen(process.env.PORT || 8080);
